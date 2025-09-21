6c8b2bf0f47db6df45766220897ab4ccdfcf10b2
teaguerchubak <teaguechubak@gmail.com>
2025-09-20 20:05:02 -0600

    Finished week 1 prove assignment

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,26 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+// Update Title //
+document.title = parkData.fullName;
+
+// Update Hero Image //
+const heroImage = document.querySelector(".hero-img-container > img");
+heroImage.src = parkData.images[0].url;
+
+// Update Hero Image Text //
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+function parkInfoTemplate(info) {
+    return `<a href="/" class="hero-img-title">${info.name}</a>
+    <p class="hero-img-subtitle">
+        <span>${info.designation}</span>
+        <span>${info.states}</span>
+    </p>`;
+}
+
+document.querySelector(".hero-img-text").innerHTML = parkInfoTemplate(parkData);
+
